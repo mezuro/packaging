8299b27194810a0fd7a2b859b7f3a3ca1aa99097
Heitor Reis <marcheing@gmail.com>
2015-10-26 14:35:49 -0200

    Add Procfile to the run script

--- scripts/kalibro_configurations/run.sh
+++ scripts/kalibro_configurations/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd /root/mezuro/pkgs/kalibro_configurations
+echo "web: bundle exec rails s -p 8083" > /root/mezuro/src/kalibro_configurations/Procfile
 pkgr package /root/mezuro/src/kalibro_configurations &> log
