951ec724236d76fe0d9007bc155aa366eb5e2863
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-10-26 15:02:34 -0200

    Change Procfile creation behaviour
    Instead of the run.sh script echo the file contents, the file is now
    part of the application scripts and is copied inside the cloned source
    that will be packaged.
    
    This gives more flexibility for future application configurations and
    extraction of Rakefile logic into modules that can be tested.

--- scripts/kalibro_configurations/run.sh
+++ scripts/kalibro_configurations/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cd /root/mezuro/pkgs/kalibro_configurations
-echo "web: bundle exec rails s -p 8083" > /root/mezuro/src/kalibro_configurations/Procfile
 pkgr package /root/mezuro/src/kalibro_configurations &> log
