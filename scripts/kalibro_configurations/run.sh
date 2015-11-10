e6edd4d0fa1b18054734b8d8ef242da8b4b8e872
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-11-10 16:10:19 -0200

    Generate RPM package

--- scripts/kalibro_configurations/run.sh
+++ scripts/kalibro_configurations/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 service postgresql start
-fpm-cook package --pkg-dir /root/mezuro/pkgs/kalibro_configurations /root/mezuro/scripts/kalibro_configurations/recipe.rb
+fpm-cook package -p centos --pkg-dir /root/mezuro/pkgs/kalibro_configurations /root/mezuro/scripts/kalibro_configurations/recipe.rb
+fpm-cook clean /root/mezuro/scripts/kalibro_configurations/recipe.rb
+fpm-cook package -p debian --pkg-dir /root/mezuro/pkgs/kalibro_configurations /root/mezuro/scripts/kalibro_configurations/recipe.rb
