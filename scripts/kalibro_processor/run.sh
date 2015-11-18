9ac2c549d22da64055a2f83c0d77a1911fce3ddb
Diego Araújo <diegoamc90@gmail.com>
2015-11-18 16:38:37 -0200

    Update Prezento packaging to depend on built packages
    - Split version numbers to a separate file so they can be used in the
      Rakefile. Actually using them in the recipes is still pending.
    - Create file tasks for the DEB files and make the tasks depend on them,
      this way up-to-date packages won't be rebuilt every time
    - Remove mk_structure task in favor of directory tasks that can be
      used as dependencies
    - Rename package result directories to use dashes instead of underlines
    - Update Prezento build command to install dependent packages
    
    Signed off by: Eduardo Araújo <duduktamg@hotmail.com>
    Signed off by: Diego Araújo <diegoamc90@gmail.com>

--- scripts/kalibro_processor/run.sh
+++ scripts/kalibro_processor/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 systemctl start postgresql
-fpm-cook package --pkg-dir /root/mezuro/pkgs/kalibro_processor /root/mezuro/scripts/kalibro_processor/recipe.rb
+fpm-cook package --pkg-dir /root/mezuro/pkgs/kalibro-processor /root/mezuro/scripts/kalibro_processor/recipe.rb
--- scripts/prezento/run.sh
+++ scripts/prezento/run.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-systemctl start postgresql
+set -ex
+
+# systemctl start postgresql
+
+if which yum >/dev/null 2>&1; then
+  yum install -y "$@"
+elif which dpkg >/dev/null 2>&1; then
+  dpkg -i "$@"
+else
+  echo "Unknown operating system" >&2
+  exit 1
+fi
+
 fpm-cook package --pkg-dir /root/mezuro/pkgs/prezento /root/mezuro/scripts/prezento/recipe.rb
