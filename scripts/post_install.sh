de77492d165a12b2316efa992bf24e5ce185607f
Daniel Miranda <danielkza2@gmail.com>
2015-11-26 16:51:57 -0200

    Improve post_install script
    - Improve reliability by making commands that shouldn't fail fail the
    whole script, with manual exceptions for the database and service start
    commands
    - Improve readability by splitting some long commands into structured
    blocks
    - Only run the database commands if we could start postgresql
    - Use `systemctl is-active` command instead of grepping the output of
    `systemctl status`
    - Call out to the administration script to run bundle commands
    - Improve the WAS_RUNNING check, it was confusing due to setting the
    variable to 0 when it was actually representing 'true'.

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
+set -e
+
 name=
+username=
 port=
 homedir=
 configdir=
-username=
+
+admin_bin="${name}-admin"
 
 # Create system user
 # Based on: https://github.com/salizzar/gordon/blob/master/lib/gordon/cookery/application_user.rb
-/usr/bin/getent group ${username} >/dev/null || /usr/sbin/groupadd --system ${username}
-/usr/bin/getent passwd ${username} >/dev/null || ( (/usr/sbin/useradd --system --gid ${username} --home-dir ${homedir} --shell /sbin/nologin ${username} >/dev/null || :; ) && chown -R ${username}:${username} ${homedir} )
-
-# Create postgresql user
-systemctl start postgresql
-sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='${username}'" | grep -q 1 || sudo -u postgres psql -c 'create role ${username} with createdb login'
+getent group ${username} >/dev/null || groupadd --system ${username}
+if ! getent passwd ${username} >/dev/null; then
+  useradd --system --gid ${username} --home-dir ${homedir} --shell /sbin/nologin ${username} >/dev/null
+  chown -R ${username}:${username} ${homedir}
+fi
 
 cd ${homedir}
 # Install gems from cache
@@ -21,34 +24,39 @@ bundle install --deployment --without development:test > /dev/null
 
 # Check if the service is running
 WAS_RUNNING=0
-if [ `systemctl status ${name}.target | grep running > /dev/null` ]; then
+if systemctl is-active --quiet ${name}.target; then
   systemctl stop ${name}.target
   WAS_RUNNING=1
 fi
-
-# Setup database
-if ! [ `sudo -u postgres psql -lqt | cut -d '|' -f 1 | grep -w ${username}_production` ]; then
-  sudo -u ${username} RAILS_ENV=production bundle exec rake db:create > /dev/null
-  echo "\n"
-  echo "${name} database successfully created. If you want to populate it with default data, please run the following commands:"
-  echo "\n"
-  echo "\t cd ${homedir}"
-  echo "\t sudo -u ${username} RAILS_ENV=production bundle exec rake db:seed"
-  echo "\n"
-  echo "NOTICE: errors may be risen if the required services are not running."
-  echo "\n"
-fi
-sudo -u ${username} RAILS_ENV=production bundle exec rake db:migrate > /dev/null
 
 # Create systemd scripts
 bundle exec foreman export systemd -a ${name} -u ${username} -p ${port} /usr/lib/systemd/system
 sed -i 's/StopWhenUnneeded=true/StopWhenUnneeded=false/g' /usr/lib/systemd/system/${name}.target
 
-# Restart the service if it was running
-if [ $WAS_RUNNING -ne 0 ]; then
-  systemctl start ${name}.target
-fi
-
 # Create secret token configuration file
 SECRET=$(bundle exec rake secret)
-sed -i 's/<%= ENV\["SECRET_KEY_BASE"\] %>/'"$SECRET"'/g' ${configdir}/secrets.yml
+sed -i "s/<%= ENV\[\"SECRET_KEY_BASE\"\] %>/$SECRET/g" ${configdir}/secrets.yml
+
+# Create postgresql user
+if systemctl start postgresql; then
+  sleep 2
+  if ! sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='${username}'" | grep -q 1; then
+    sudo -u postgres psql -c "create role ${username} with createdb login" || :
+  fi
+
+  # Setup database
+  if ! sudo -u postgres psql -lqt | cut -d '|' -f 1 | grep -qw "${username}_production"; then
+    $admin_bin rake db:create > /dev/null || :
+    $admin_bin rake db:migrate > /dev/null || :
+
+    cat <<EOF
+${name} database successfully created. If you want to populate it with default data, please run `$admin_bin rake db:seed`.
+NOTICE: errors may be risen if the required services are not running.
+EOF
+  fi
+fi
+
+# Restart the service if it was running
+if [ $WAS_RUNNING -ne 0 ]; then
+  systemctl start ${name}.target || :
+fi
