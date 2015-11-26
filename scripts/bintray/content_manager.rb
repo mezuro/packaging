7cd4c15c4eb9264c2b64c5cf694c09706459a03d
Heitor Reis <marcheing@gmail.com>
2015-11-26 15:33:04 -0200

    Fix put requests on content manager
    Signed off by: Diego Araújo <diegoamc90@gmail.com>

--- scripts/bintray/content_manager.rb
+++ scripts/bintray/content_manager.rb
@@ -5,14 +5,14 @@ class ContentManager
   end
 
   def upload(repo, package, version, file_path)
-    RequestMaker.put("/content/#{@username}/#{repo}/#{package}/#{version}/#{file_path}", @username, @key, attributes)
+    RequestMaker.put("/content/#{@username}/#{repo}/#{package}/#{version}/#{file_path}", @username, @key)
   end
 
   def debian_upload(repo, package, version, file_path, debian_info)
-    RequestMaker.put("/content/#{@username}/#{repo}/#{package}/#{version}/#{file_path};
-                      deb_distribution=#{debian_info[:distros]};
-                      deb_component=#{debian_info[:components]};
-                      deb_architecture=#{debian_info[:archs]}", @username, @key, attributes)
+    RequestMaker.put("/content/#{@username}/#{repo}/#{package}/#{version}/#{file_path};" \
+                      "deb_distribution=#{debian_info[:distros]};" \
+                      "deb_component=#{debian_info[:components]};" \
+                      "deb_architecture=#{debian_info[:archs]}", @username, @key)
   end
 
   def publish(repo, package, version, discard = 0)
