afd2075cb04c5bce66671048b18478b0a9c64431
Diego Araújo <diegoamc90@gmail.com>
2015-12-18 19:33:57 -0200

    Add new tag and version for prezento-spb

--- scripts/prezento_spb/recipe.rb
+++ scripts/prezento_spb/recipe.rb
@@ -3,7 +3,7 @@ require_relative '../prezento/recipe'
 class PrezentoSPB < Prezento
   name     MezuroInformations::PREZENTO_SPB[:data][:name]
   version  MezuroInformations::PREZENTO_SPB[:info][:version]
-  source   MezuroInformations::PREZENTO_SPB[:data][:vcs_url], :with => :git, :tag => "v#{version}.colab"
+  source   MezuroInformations::PREZENTO_SPB[:data][:vcs_url], :with => :git, :tag => "v#{version}.colab3"
 
   maintainer  'Mezuro Team <mezurometrics@gmail.com>'
   license     MezuroInformations::PREZENTO_SPB[:data][:licenses][0]
