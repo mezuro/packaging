95ed162fe69faf1d4fcd0f8a2b4d7b8ee47f2566
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2016-01-06 08:40:13 -0200

    Bump releases in order to get the correct postinst

--- mezuro_informations.rb
+++ mezuro_informations.rb
@@ -1,5 +1,5 @@
 module MezuroInformations
-  KALIBRO_PROCESSOR = { info: { version: '1.1.5', release: '3' },
+  KALIBRO_PROCESSOR = { info: { version: '1.1.5', release: '4' },
                         data: { name: 'kalibro-processor',
                                 desc: 'Web service for static source code analysis',
                                 labels: ['web service', 'code analysis', 'source code metrics'],
@@ -9,7 +9,7 @@ module MezuroInformations
                                 issue_tracker_url: 'https://github.com/mezuro/kalibro_processor/issues',
                                 public_download_numbers: true }
                       }
-  KALIBRO_CONFIGURATIONS = { info: { version: '1.2.5', release: '2' },
+  KALIBRO_CONFIGURATIONS = { info: { version: '1.2.5', release: '3' },
                              data: { name: 'kalibro-configurations',
                                      desc: 'Web service for managing code analysis configurations',
                                      labels: ['web service', 'source code metrics', 'metric configurations'],
@@ -19,7 +19,7 @@ module MezuroInformations
                                      issue_tracker_url: 'https://github.com/mezuro/kalibro_configurations/issues',
                                      public_download_numbers: true }
                            }
-  PREZENTO = { info: { version: '0.10.0', release: '2' },
+  PREZENTO = { info: { version: '0.10.0', release: '3' },
                data: { name: 'prezento',
                        desc: 'Collaborative code metrics',
                        labels: ['web interface', 'source code metrics'],
