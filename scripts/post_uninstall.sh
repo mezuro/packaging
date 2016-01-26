f3b7043872a1063a4b307254bfbe7530a44f9e84
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2016-01-26 16:07:31 -0200

    Fix postuninstall check for upgrade on CentOS
    RPM does not work as we expected, actually passing as argument 0 for
    removals and 1 for upgrades.

--- scripts/post_uninstall.sh
+++ scripts/post_uninstall.sh
@@ -4,6 +4,6 @@ set -e
 
 homedir=
 
-if ! [[ -z "$1" ]] && ! [[ "$1" == upgrade ]] && ! [[ "$1" -ge 2 ]]; then
+if ! [[ -z "$1" ]] && ! [[ "$1" == upgrade ]] && ! [[ "$1" -eq 1 ]]; then
   rm -rf ${homedir}
 fi
